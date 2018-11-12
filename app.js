6bfb61db8a11424858be92d7ce40ad7b2a1cbb6e
v123582 <v123582@gmail.com>
2018-11-12 13:56:19 +0800

    update "post /create" route to get variable from req.body

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   let name = req.query.name || 'World'
   res.render('index', {name : name})
+})
+
+app.post('/create', (req, res) => {
+  let item = req.body.item
+  res.send(item)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
