94ca2ef68ce9dc2f6f1ab70b37fcbbd26533df35
v123582 <v123582@gmail.com>
2018-11-12 13:59:57 +0800

    connect to mongo using mongoose

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/l2-express-tutoial', { useNewUrlParser: true }, (err, db) => {
+  if (err) console.log(`Error`, err)
+  console.log(`Connected to MongoDB`)
+})
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
